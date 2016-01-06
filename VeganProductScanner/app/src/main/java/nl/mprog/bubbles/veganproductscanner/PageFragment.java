d0ff1026af620e1fc510101b9ea785e89af34d65
Victor <vdenhaan@gmail.com>
2016-01-06 16:37:41 +0100

    added tabs, fragments and basic layout of some fragments

--- VeganProductScanner/app/src/main/java/nl/mprog/bubbles/veganproductscanner/PageFragment.java
+++ VeganProductScanner/app/src/main/java/nl/mprog/bubbles/veganproductscanner/PageFragment.java
@@ -1,7 +1,45 @@
 package nl.mprog.bubbles.veganproductscanner;
 
-/**
- * Created by Bubbles on 06/01/2016.
- */
-public class PageFragment {
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+// In this case, the fragment displays simple text based on the page
+public class PageFragment extends Fragment {
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int mPage;
+
+    public static PageFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        PageFragment fragment = new PageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view;
+        if (mPage == 2) {
+            view = inflater.inflate(R.layout.search_fragment, container, false);
+        }
+        else if (mPage == 3) {
+            view = inflater.inflate(R.layout.info_fragment, container, false);
+        }
+        else {
+            view = inflater.inflate(R.layout.result_fragment, container, false);
+        }
+        return view;
+    }
 }
--- VeganProductScanner/app/src/main/java/nl/mprog/bubbles/veganproductscanner/SampleFragmentPagerAdapter.java
+++ VeganProductScanner/app/src/main/java/nl/mprog/bubbles/veganproductscanner/SampleFragmentPagerAdapter.java
@@ -1,7 +1,33 @@
 package nl.mprog.bubbles.veganproductscanner;
 
-/**
- * Created by Bubbles on 06/01/2016.
- */
-public class SampleFragmentPagerAdapter {
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Tab 1", "Tab 2", "Tab 3" };
+    Context context;
+
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
 }

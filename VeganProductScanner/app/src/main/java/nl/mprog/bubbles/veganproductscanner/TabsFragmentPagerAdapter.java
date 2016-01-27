4b097df34e04fcc4e2abce701699ea87dbe98a77
Victor <vdenhaan@gmail.com>
2016-01-27 23:30:29 +0100

    renamed SampleFragmentPagerAdapter to TabsFragmentPagerAdapter

--- VeganProductScanner/app/src/main/java/nl/mprog/bubbles/veganproductscanner/TabsFragmentPagerAdapter.java
+++ VeganProductScanner/app/src/main/java/nl/mprog/bubbles/veganproductscanner/TabsFragmentPagerAdapter.java
@@ -9,15 +9,20 @@ import android.support.v4.app.FragmentPagerAdapter;
  * Victor den Haan - 10118039 - vdenhaan@gmail.com
  * Based on code at
  * https://github.com/codepath/android_guides/wiki/Google-Play-Style-Tabs-using-TabLayout
+ *
+ * TabsFragmentPagerAdapter links the Tabs to the Fragments. The user can tap the tabs to move to
+ * their corresponding fragments, but sliding also works because of the linked
+ * TabLayout/FragmentManager which is taking care of in the onCreate of MainActivity.
+ * TODO Change tabTitles to Icons with text
  */
 
-public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+public class TabsFragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
     final int PAGE_COUNT = 3;
     private String tabTitles[] = new String[] { "Result", "Search", "Info"};
     Context context;
     MainActivity mainActivity;
 
-    public SampleFragmentPagerAdapter(FragmentManager fm, Context context, MainActivity activity) {
+    public TabsFragmentPagerAdapter(FragmentManager fm, Context context, MainActivity activity) {
         super(fm);
         mainActivity = activity;
         this.context = context;
@@ -28,14 +33,15 @@ public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
         return PAGE_COUNT;
     }
 
+    /** determines the fragment corresponding to each tab */
     @Override
     public Fragment getItem(int position) {
         return PageFragment.newInstance(position + 1, mainActivity);
     }
 
+    /** generates the title for each tab */
     @Override
     public CharSequence getPageTitle(int position) {
-        // Generate title based on item position
         return tabTitles[position];
     }
 }

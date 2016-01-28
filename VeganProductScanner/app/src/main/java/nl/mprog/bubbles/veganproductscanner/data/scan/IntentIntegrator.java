3b2eabe99f9b0af6f8ef5168835aad58624dad0b
Victor <vdenhaan@gmail.com>
2016-01-28 19:15:01 +0100

    added TODO

--- VeganProductScanner/app/src/main/java/nl/mprog/bubbles/veganproductscanner/data/scan/IntentIntegrator.java
+++ VeganProductScanner/app/src/main/java/nl/mprog/bubbles/veganproductscanner/data/scan/IntentIntegrator.java
@@ -2,6 +2,11 @@ package nl.mprog.bubbles.veganproductscanner.data.scan;
 
 /**
  * Victor den Haan - 10118039 - vdenhaan@gmail.com
+ *
+ * IntentIntegrator handles the
+ *
+ * TODO Build the scanner into the app, so it doesn't require another download and needs less
+ * permissions because of stripped functions
  */
 
 /**
